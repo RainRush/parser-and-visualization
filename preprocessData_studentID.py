16961025ce334898d3eccec4a1d69023943b396a
Daniel Hu <daniel.hu@myzeller.com>
2022-10-08 20:30:06 +1100

    feat: data preprocessing

--- preprocessData_studentID.py
+++ preprocessData_studentID.py
@@ -3,9 +3,34 @@ import re
 POST_TYPE_ID_ATTRIBUTE = "PostTypeId"
 BODY_ATTRIBUTE = "Body"
 
+def trancateWhitespace(line):
+	line = re.sub("\s+", " ", line)
+	return line
+
+def removeHTMLTags(line):
+	line = re.sub("<.*?>", "", line)
+	return line
+
+def removeUnneededCharRef(line):
+	line = re.sub("&#xA;", " ", line)
+	line = re.sub("&#xD;", " ", line)
+	return line
+
+def convertCharRefToOriginalForm(line):
+	line = re.sub("&amp;", "&", line)
+	line = re.sub("&quot;", "\"", line)
+	line = re.sub("&apos;", "'", line)
+	line = re.sub("&gt;", ">", line)
+	line = re.sub("&lt;", "<", line)
+	return line
+
 def preprocessLine(inputLine):
-	postTypeIdSearchResult = re.search(f'{BODY_ATTRIBUTE}=\"(.*?)\"', inputLine)
-	preprocessedLine = postTypeIdSearchResult.group(1)
+	bodyValueSearchResult = re.search(f'{BODY_ATTRIBUTE}=\"(.*?)\"', inputLine)
+	preprocessedLine = bodyValueSearchResult.group(1)
+	preprocessedLine = convertCharRefToOriginalForm(preprocessedLine)
+	preprocessedLine = removeUnneededCharRef(preprocessedLine)
+	preprocessedLine = removeHTMLTags(preprocessedLine)
+	preprocessedLine = trancateWhitespace(preprocessedLine)
 	return preprocessedLine
 
 def getPostTypeId(inputLine):
@@ -16,23 +41,29 @@ def writeLineToFile(outputFile, line):
 	with open(outputFile, 'a', encoding='utf-8') as fileToWrite:
 		fileToWrite.write(f"{line}\n")
 
+def resetOutputFiles(outputFiles):
+	for outputFile in outputFiles:
+		with open(outputFile, 'w') as _:
+			pass
+
 def splitFile(inputFile, outputFile_question, outputFile_answer):
+	resetOutputFiles([outputFile_question, outputFile_answer])
 	file = open(inputFile, 'r')
-	# to delete
-	lineCursor = 0
+
 	for line in file:
 		if (POST_TYPE_ID_ATTRIBUTE not in line or BODY_ATTRIBUTE not in line):
 			continue
-		# hold this line for test purpose, lineCursor to be removed
-		if (lineCursor < 5):
-			preprocessedLine = preprocessLine(line)
-			postTypeId = getPostTypeId(line)
+		
+		preprocessedLine = preprocessLine(line)
+		if (preprocessedLine == ""):
+			continue
 
-			if (postTypeId == "1"):
-				writeLineToFile(outputFile_question, preprocessedLine)
-			if (postTypeId == "2"):
-				writeLineToFile(outputFile_answer, preprocessedLine)
-		lineCursor += 1
+		postTypeId = getPostTypeId(line)
+		if (postTypeId == "1"):
+			writeLineToFile(outputFile_question, preprocessedLine)
+		if (postTypeId == "2"):
+			writeLineToFile(outputFile_answer, preprocessedLine)
+		
 	file.close()
 
 if __name__ == "__main__":
